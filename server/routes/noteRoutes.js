c59af65910a2924d59e5975df6a5344e8ea0b2e2
Jobanpreet <jobanjatt113@gmail.com>
2023-08-28 15:11:35 +0530

    users stories compeleted

---
+++ server/routes/noteRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const notesController = require('../controllers/notesController')
+const verifyJwt = require('../middleware/verifyJwt')
+
+router.use(verifyJwt)
+
+router.route('/')
+.get(notesController.getAllNotes)
+.post(notesController.createNewNote)
+.patch(notesController.updateNote)
+.delete(notesController.deleteNote)
+
+module.exports = router;
